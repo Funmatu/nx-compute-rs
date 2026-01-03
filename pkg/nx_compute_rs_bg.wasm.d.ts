ab2cfe6079047d3c8b702dd7964e7e3b97350231
Funmatu <Funmatu@users.noreply.github.com>
2026-01-03 11:03:17 +0000

    deploy: 25cb548992173796cba995756a9a3b804d2d8ccc

---
+++ pkg/nx_compute_rs_bg.wasm.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const compute_metrics_js: (a: bigint, b: number, c: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_start: () => void;
